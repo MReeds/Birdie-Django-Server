5892f40f89d844365cfe71a46887319e60aee1f8
MReeds <mattlreeds@gmail.com>
2020-07-22 19:01:42 -0500

    initial setup

---
+++ BirdieEnv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/matt_reeds/NSS/BirdieServer/BirdieEnv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ BirdieServer/BirdieAPI/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BirdieapiConfig(AppConfig):
+    name = 'BirdieAPI'
